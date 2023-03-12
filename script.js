a6971a5afa2fcd60a7bff4beccb74affa597069a
Anthony <semprini.anthony@gmail.com>
2023-03-12 21:41:44 +0100

    relie button html a function sauter script

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var perso = document.querySelector(".perso");
 var obstacles = document.querySelector(".obstacles");
 
-function sauter()
+function sauter(){
+    alert  ("vous avez cliquez sur le boutton")
+};
